efab6037ab62bf45da39a75d3ad72341d0f8842e
Thomas Harmeyer <thomasmharmeyer@gmail.com>
2021-11-03 13:39:10 -0400

    Fixed navbar on mobile

--- src/components/BottomBar.jsx
+++ src/components/BottomBar.jsx
@@ -1,20 +1,23 @@
-import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
 import Row from "react-bootstrap/Row";
-
 const BottomBar = () => {
   return (
     <footer>
-      <Navbar variant="dark" bg="dark">
-        <Container fluid className="justify-content-md-center">
-          <Navbar.Brand>
-            <Row>
-              <Col>Thomas Harmeyer</Col>
-              <Col>thomasmharmeyer@gmail.com</Col>
-              <Col>262-444-2422</Col>
-            </Row>
-          </Navbar.Brand>
+      <Navbar expand="sm" bg="dark" fixed="bottom" variant="dark">
+        <Container className="justify-content-md-center">
+          <Row>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="">
+                <NavDropdown.Item>Thomas Harmeyer</NavDropdown.Item>
+                <NavDropdown.Item>thomasmharmeyer@gmail.com</NavDropdown.Item>
+                <NavDropdown.Item>262-444-2422</NavDropdown.Item>
+              </Nav>
+            </Navbar.Collapse>
+          </Row>
         </Container>
       </Navbar>
     </footer>
