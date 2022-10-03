d5f680bce2f152b5c12c630ba811864aabea9ea9
Thomas Harmeyer <thomasmharmeyer@gmail.com>
2022-10-03 15:09:38 -0400

    added google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import "./App.css";
 import About from "./components/Projects";
 import Home from "./components/Home";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import TopBar from "./components/TopBar";
 import Container from "react-bootstrap/esm/Container";
 import BottomBar from "./components/BottomBar";
 import BFS from "./components/algorithms/BFS";
 import DFS from "./components/algorithms/DFS";
+import ReactGA from "react-ga";
+ReactGA.initialize("G-353FEKHY1Q");
+ReactGA.pageview(window.location.pathname + window.location.search);
 function App() {
   return (
     <Router>
